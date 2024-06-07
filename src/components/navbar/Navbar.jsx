22903d4ffed2ce3697150e18f4d37ee91a84d61b
shara <chndshara@gmail.com>
2024-06-07 20:23:34 +0530

    mobile responsive and mobile menu down fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaRegUser, GoProjectRoadmap, IoDocumentTextOutline, MdOutlineContactPag
 import { Link, useLocation } from 'react-router-dom'
 
 
-function Navbar() {
+function Navbar(props) {
     const path = useLocation();
     const [activeTab, setActiveTab] = useState('');
 
@@ -13,40 +13,63 @@ function Navbar() {
         setActiveTab(CurrentTab)
     }, [path])
 
+    console.log()
 
 
     return (
         <>
-            <nav className='bg-white rounded-xl'>
-                <ul className='flex space-x-6 px-5 py-4  text-gray-700'>
+            <nav className='bg-white rounded-xl  lg:max-w-[51%]'>
+                <ul className='flex space-x-6 px-3 py-4  text-gray-700'>
                     <Link to='/'>
-                        <li className={`w-[85px] rounded-xl  bg-gray-100 space-y-2 h-[85px] flex flex-col items-center justify-center ${activeTab === 'about' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`} >
-                            <FaRegUser size={28} />
-                            <span className='text-md font-semibold'>About</span>
+                        <li className={`${props && props?.size ? "py-2 px-[10px]": " py-3 px-5"}  rounded-xl  bg-gray-100 space-y-2  flex flex-col items-center justify-center ${activeTab === 'about' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`} >
+                           {
+                            props?.size ?  <FaRegUser size={20} /> :  <FaRegUser size={28} />
+                           }
+                          {
+                            props?.size ? <span className='text-sm font-semibold'>About</span> : <span className='text-md font-semibold'>About</span>
+                          }  
                         </li>
                     </Link>
                     <Link to='/resume'>
-                        <li className={`w-[85px] rounded-xl  bg-gray-100 space-y-2 h-[85px] flex flex-col items-center justify-center ${activeTab === 'resume' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
-                            <IoDocumentTextOutline size={28} />
-                            <span className='text-md font-semibold'>Resume</span>
+                        <li className={`${props && props?.size ? "py-2 px-[5px]" : "p-3"}  rounded-xl  bg-gray-100 space-y-2  flex flex-col items-center justify-center ${activeTab === 'resume' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
+                            {/* <IoDocumentTextOutline size={28} /> */}
+                            {
+                            props?.size ?  <IoDocumentTextOutline size={20} /> :  <IoDocumentTextOutline size={28} />
+                           }
+                            {
+                            props?.size ? <span className='text-sm font-semibold'>Resume</span> : <span className='text-md font-semibold'>Resume</span>
+                          } 
+                            {/* <span className='text-md font-semibold'>Resume</span> */}
                         </li>
                     </Link>
 
                     <Link to='/projects'>
-                        <li className={`w-[85px] rounded-xl  bg-gray-100 space-y-2 h-[85px] flex flex-col items-center justify-center ${activeTab === 'projects' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
-                            <GoProjectRoadmap size={28} />
-                            <span className='text-md font-semibold'>Projects</span>
+                        <li className={`${props && props?.size ? "py-2 px-[5px]" : "p-3"}  rounded-xl  bg-gray-100 space-y-2  flex flex-col items-center justify-center ${activeTab === 'projects' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
+                            {/* <GoProjectRoadmap size={28} /> */}
+                            {
+                            props?.size ?  <GoProjectRoadmap size={20} /> :  <GoProjectRoadmap size={28} />
+                           }
+                            {
+                            props?.size ? <span className='text-sm font-semibold'>Projects</span> : <span className='text-md font-semibold'>Projects</span>
+                          } 
+                            {/* <span className='text-md font-semibold'>Projects</span> */}
                         </li>
                     </Link>
 
                     <Link to='/contact'>
-                        <li className={`w-[85px] rounded-xl  bg-gray-100 space-y-2 h-[85px] flex flex-col items-center justify-center ${activeTab === 'contact' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
-                            <MdOutlineContactPage size={28} />
-                            <span className='text-md font-semibold'>Contact</span>
+                        <li className={`${props && props?.size ? "py-2 px-[5px]" : "p-3"}  rounded-xl  bg-gray-100 space-y-2  flex flex-col items-center justify-center ${activeTab === 'contact' ? 'bg-gradient-to-tr from-sky-400 to-sky-500 text-white':'' }  transition-colors hover:text-white duration-500 hover:bg-gradient-to-tr from-sky-400 to-sky-500`}>
+                            {/* <MdOutlineContactPage size={28} /> */}
+                            {
+                            props?.size ?  <MdOutlineContactPage size={20} /> :  <MdOutlineContactPage size={28} />
+                           }
+                            {
+                            props?.size ? <span className='text-sm font-semibold'>Contact</span> : <span className='text-md font-semibold'>Contact</span>
+                          } 
+                            {/* <span className='text-md font-semibold'>Contact</span> */}
                         </li>
                     </Link>
 
-                    {/* <li className='w-[85px] rounded-xl  bg-gray-100 space-y-2 h-[85px] flex flex-col items-center justify-center'>
+                    {/* <li className='w-[85px] h-[85px] rounded-xl  bg-gray-100 space-y-2  flex flex-col items-center justify-center'>
                         <FaRegUser size={28}/>
                     <span className='text-md font-semibold capitalize'>about</span> 
                     </li> */}
--- src/layouts/Index.jsx
+++ src/layouts/Index.jsx
@@ -1,37 +1,54 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import { Sidebar, Navbar } from '../components/index'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Dialog from '../components/Dialog/Dialog';
+
 function Index() {
+  const location = useLocation();
+const [isMobile, setIsMobile] = useState('');
+const sizeHandler = () => {
+  if(window.innerWidth < 750 ){
+    setIsMobile(true)
+  }else {
+    setIsMobile(false)
+  }
+}
 
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+useEffect(()=>{
+  window.addEventListener('resize', sizeHandler)
+})
 
-  const openDialog = () => {
-    setIsDialogOpen(true);
-};
-
-const closeDialog = () => {
-    setIsDialogOpen(false);
-};
+console.log(location.pathname)
 
   return (
     <>
       <div className='lg:flex lg:space-x-2 bg-gray-200 lg:px-12 px-3 lg:py-[16rem] py-10 space-y-4'>
-        <div  className='lg:w-1/3 lg:max-h-[750px] lg:mx-6 pb-8 text-black border-r-2 rounded-xl bg-white border-gray-100 lg:sticky top-[10rem]'>
-          <Sidebar  />
-          <button onClick={openDialog}>open</button>
-        </div>
-        <div className='lg:w-2/3 lg:flex flex-col sm:px-3  lg:min-h-[100vh]'>
+        
+         
+          {
+            isMobile && location.pathname === "/" ? <div  className='lg:w-1/3 lg:max-h-[750px] lg:mx-6 pb-8 text-black border-r-2 rounded-xl bg-white border-gray-100 lg:sticky top-[10rem]'> <Sidebar  />  </div> 
+            : !isMobile ? <div  className='lg:w-1/3 lg:max-h-[750px] lg:mx-6 pb-8 text-black border-r-2 rounded-xl bg-white border-gray-100 lg:sticky top-[10rem]'> <Sidebar  />  </div>: ""
+          }
+          {/* <button onClick={openDialog}>open</button> */}
+       
+        <div className='lg:w-2/3 flex flex-col sm:px-3  lg:min-h-[100vh]'>
           <ul>
-            <li className='mx-0 px-10 py-4 lg:min-h-12  justify-end hidden lg:visible'><Navbar /></li>
-            <li><Outlet /></li>
-            <Dialog isOpen={isDialogOpen} onClose={closeDialog}>
+            {
+              isMobile ? '' : <li className='py-4 lg:min-h-12 flex justify-end'><Navbar size={isMobile} /></li>
+            }
+            
+            <li><Outlet context={[isMobile]} /></li>
+            
+            {/* <Dialog isOpen={isDialogOpen} onClose={closeDialog}>
                 <h2>Dialog Title</h2>
                 <p>This is a simple dialog.</p>
-            </Dialog>
+            </Dialog> */}
           </ul>
         </div>
       </div>
+      {
+              isMobile ? <li className='flex justify-center mt-2 mb-0 sticky bottom-0'><Navbar size={isMobile} /></li> : ""
+            }
     </>
   )
 }
